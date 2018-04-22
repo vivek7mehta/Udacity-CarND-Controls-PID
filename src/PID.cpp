905fc93d1f6fc77b185f8a56d7e4346eee682a94
vivek mehta <vivek7mehta@gmail.com>
2018-04-22 23:04:18 +0530

    PID control project

---
+++ src/PID.cpp
@@ -0,0 +1,29 @@
+#include "PID.h"
+
+using namespace std;
+
+/*
+* TODO: Complete the PID class.
+*/
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double kp, double ki, double kd) {
+    Kp = kp;
+    Ki = ki;
+    Kd = kd;
+    i_error = 0;
+}
+
+void PID::UpdateError(double cte) {
+
+    d_error = cte - p_error;
+    p_error = cte;
+    i_error+= p_error;
+}
+
+double PID::TotalError() {
+    return -Kp * p_error - Kd * d_error - Ki * i_error;
+}
